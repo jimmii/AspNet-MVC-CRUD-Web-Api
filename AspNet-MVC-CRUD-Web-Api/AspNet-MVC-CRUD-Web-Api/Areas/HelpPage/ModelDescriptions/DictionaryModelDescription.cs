fb14e00a33644e04e6f944c200ce299f67d8b866
jimmii <jimmii@o2.pl>
2019-06-23 22:08:03 +0200

    Solution created

---
+++ AspNet-MVC-CRUD-Web-Api/AspNet-MVC-CRUD-Web-Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AspNet_MVC_CRUD_Web_Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AspNet-MVC-CRUD-Web-Api/AspNet-MVC-CRUD-Web-Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNet_MVC_CRUD_Web_Api.WebApiApplication" Language="C#" %>
