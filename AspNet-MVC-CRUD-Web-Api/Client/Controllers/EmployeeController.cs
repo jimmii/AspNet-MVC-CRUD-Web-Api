20c8193df2e7418ad95fa0f1651589caa0a69ff3
jimmii <jimmii@o2.pl>
2019-06-23 23:49:28 +0200

    Client List

---
+++ AspNet-MVC-CRUD-Web-Api/Client/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Client.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<EmployeeModel> emplist;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee").Result;
+            emplist = response.Content.ReadAsAsync<IEnumerable<EmployeeModel>>().Result;
+            return View(emplist);
+        }
+    }
+}
---
+++ AspNet-MVC-CRUD-Web-Api/Client/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Client.Models
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Email { get; set; }
+        public Nullable<int> PhoneNumber { get; set; }
+    }
+}
